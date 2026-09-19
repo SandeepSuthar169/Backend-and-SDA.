137beb1f8a7729399799ce5a70d0159ddf67f325
Sandeep Suthar <ss.suthar.004@gmail.com>
2026-09-20 00:55:28 +0530

    feat: update PostgreSQL configuration and add initial SQL scripts

---
+++ 05 PostgreSQL/test.sql
@@ -0,0 +1,21 @@
+-- CREATE TABLE inventories (
+--     name VARCHAR(255),
+--     brand VARCHAR(50),
+--     quantity INT,
+--     price DECIMAL(19, 2)
+-- );
+
+INSERT INTO inventories (name, brand, quantity, price)
+VALUES
+    ('G15 laptop', 'Dell', 9, 200.99),
+    ('I Phone 16', 'Iphone', 8, 200.99),
+    ('A18', 'Samsung', 7, 200.99);
+
+-- CREATE TABLE studentRegister (
+--     studentName VARCHAR(255),
+--     class INT,
+--     subjectCount INTEGER,
+--     classRank VARCHAR(50),
+--     pass BOOLEAN
+-- );
+
